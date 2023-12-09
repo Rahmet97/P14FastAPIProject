b88a8022291f9017c96a7a2234c0c8eba71983c1
user <user@linux.kali>
2023-12-09 15:45:43 +0500

    Models created

--- main.py
+++ main.py
@@ -5,38 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, select
 
 from database import get_async_session
-from models.models import blogs
 from schemas import BlogSchema, BlogSchemaCreate
 from auth.auth import register_router
 
 app = FastAPI(title='P14Project', version='1.0.0')
 router = APIRouter()
 
-
-@router.post('/blogs')
-async def add_blog(new_blog: BlogSchemaCreate, session: AsyncSession = Depends(get_async_session)):
-    query = insert(blogs).values(**dict(new_blog))
-    await session.execute(query)
-    await session.commit()
-    return {'success': True}
-
-
-@router.get('/blogs', response_model=List[BlogSchema])
-async def blog_list(session: AsyncSession = Depends(get_async_session)):
-    try:
-        query = select(blogs)
-        result = await session.execute(query)
-        return result.all()
-    except Exception:
-        raise HTTPException(status_code=500)
-
-
-@router.get('/blogs/{blog_id}', response_model=BlogSchema)
-async def blog_detail(blog_id: int, session: AsyncSession = Depends(get_async_session)):
-    query = select(blogs).where(blogs.c.id == blog_id)
-    result = await session.execute(query)
-    return result.one()
-
-
-app.include_router(router)
 app.include_router(register_router)
--- models/models.py
+++ models/models.py
@@ -1,18 +1,65 @@
 from datetime import datetime
 
-from sqlalchemy import Table, Column, Integer, String, Text, MetaData, TIMESTAMP
+from sqlalchemy import (
+    Table,
+    Column,
+    Integer,
+    String,
+    Text,
+    MetaData,
+    TIMESTAMP,
+    Boolean,
+    ForeignKey,
+    DECIMAL
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import ENUM
 
 metadata = MetaData()
 
-blogs = Table(
-    'blogs',
+product_colors = Table(
+    'product_colors',
+    metadata,
+    Column('product_id', Integer, ForeignKey('product.id')),
+    Column('color_id', Integer, ForeignKey('color.id'))
+)
+
+product_sizes = Table(
+    'product_sizes',
+    metadata,
+    Column('product_id', Integer, ForeignKey('product.id')),
+    Column('size_id', Integer, ForeignKey('size.id'))
+)
+
+color = Table(
+    'color',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('title', String, nullable=False),
-    Column('description', Text, nullable=False),
-    Column('date', TIMESTAMP, default=datetime.utcnow()),
-    Column('view_count', Integer, default=0)
+    Column('name', String)
 )
+
+size = Table(
+    'size',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('size', String)
+)
+
+product = Table(
+    'product',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('brand_id', ForeignKey('brand.id')),
+    Column('name', String),
+    Column('price', DECIMAL(precision=10, scale=2)),
+    Column('quantity', Integer),
+    Column('sold_quantity', Integer),
+    Column('description', Text),
+    Column('category_id', ForeignKey('category.id'))
+)
+
+product_color_relationship = relationship("color", secondary=product_colors, backref="products")
+product_size_relationship = relationship("size", secondary=product_sizes, backref="products")
 
 users = Table(
     'users',
@@ -23,5 +70,62 @@ users = Table(
     Column('email', String),
     Column('phone', String),
     Column('username', String),
-    Column('password', String)
+    Column('password', String),
+    Column('is_superuser', Boolean, default=False)
+)
+
+brand = Table(
+    'brand',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String)
+)
+
+file = Table(
+    'file',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('file', String),
+    # Column('product_id', ForeignKey())
+)
+
+subcategory_enum = ENUM('men', 'women', 'kids', name='subcategory_enum')
+
+category = Table(
+    'category',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String),
+    Column('category', subcategory_enum)
+)
+
+subcategory = Table(
+    'subcategory',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String),
+    Column('subcategory', ForeignKey('category.id'))
+)
+
+status_enum = ENUM('delivered', 'processing', 'canceled', name='status_enum')
+
+order = Table(
+    'order',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('product_id', ForeignKey('product.id')),
+    Column('user_id', ForeignKey('users.id')),
+    Column('count', Integer),
+    Column('ordered_at', TIMESTAMP, default=datetime.utcnow),
+    Column('status', status_enum)
+)
+
+review = Table(
+    'review',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('message', Text),
+    Column('user_id', ForeignKey('users.id')),
+    Column('product_id', ForeignKey('product.id')),
+    Column('reviewed_at', TIMESTAMP, default=datetime.utcnow)
 )
